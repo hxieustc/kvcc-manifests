e414261ef11c126eef4d5b5bcf883bf342d64e6d
Harry Xie <harryx@nvidia.com>
2026-08-11 10:13:35 +0300

    minor update docker test

--- docker/tests/test-docker.sh
+++ docker/tests/test-docker.sh
@@ -18,7 +18,7 @@ docker run --rm --gpus all \
   -w /opt/kvcc \
   kvcc-custom-runtime:dev \
   python3 -m pytest \
-    tests/v1/kv_offload/kvcc-tests/kvcc-e2e/validate_kvcc_e2e.py \
+    tests/v1/kv_offload/kvcc-tests/kvcc-e2e/test_regression_transfer.py \
     --model /images/models/Qwen/Qwen3-0.6B-FP8 \
     --gpus 0,1 \
     --eager-ctrl-connect true \
